e5187b2fc526d8ea9cf22aa1725f14cce84becb5
pin-hsuan-wu <65193751+pin-hsuan-wu@users.noreply.github.com>
2020-09-15 16:16:09 -0500

    update

--- section3/task1/sec3_task1.py
+++ section3/task1/sec3_task1.py
@@ -19,32 +19,40 @@ def process_message(msg):
 bm.start_kline_socket('BTCUSDT', interval=Client.KLINE_INTERVAL_1MINUTE, callback=process_message)
 bm.start()
 
+
 #2.
+headers = ['close' ,'high' ,'low','open','volume','baseVolume','datetime']    
+df = pd.DataFrame(columns=headers)
+
 def append_df(msg):
-    headers = ['close' ,'high' ,'low','open','volume','baseVolume','datetime','x']    
-    df = pd.DataFrame(columns=headers)
-    data = pd.DataFrame({"close": [msg['k']['c']],"high": [msg['k']['h']],"low": [msg['k']['l']],"open": [msg['k']['o']],"volume": [msg['k']['v']],"baseVolume": [msg['k']['q']],"datetime": [msg['E']], "x": [msg['k']['x']]})
-    df = df.append(data, ignore_index=True)
-    df["datetime"] = pd.to_datetime(df['datetime'], unit = 'ms')
-    df["datetime"] = df["datetime"].dt.strftime('%Y-%m-%d %H:%M:%S')
-    if msg['k']['x'] == True:
+    global df
+    while msg['k']['x'] == True:
+        data = pd.DataFrame({"close": [msg['k']['c']],"high": [msg['k']['h']],"low": [msg['k']['l']],"open": [msg['k']['o']],"volume": [msg['k']['v']],"baseVolume": [msg['k']['q']],"datetime": [msg['E']]})
+        data['datetime'] = pd.to_datetime(data['datetime'], unit = 'ms')
+        data["datetime"] = data["datetime"].dt.strftime('%Y-%m-%d %H:%M:%S')
+        df = df.append(data, ignore_index=True)
         print(df)
+        break
     else:
         pass
 
 bm.start_kline_socket('BTCUSDT', interval=Client.KLINE_INTERVAL_1MINUTE, callback=append_df)
 bm.start()
 
+
 #3.
+headers = ['close' ,'high' ,'low','open','volume','baseVolume','datetime']    
+df = pd.DataFrame(columns=headers)
+
 def csv_df(msg):
-    headers = ['close' ,'high' ,'low','open','volume','baseVolume','datetime','x']    
-    df = pd.DataFrame(columns=headers)
-    if msg['k']['x'] == True:
-        data = pd.DataFrame({"close": [msg['k']['c']],"high": [msg['k']['h']],"low": [msg['k']['l']],"open": [msg['k']['o']],"volume": [msg['k']['v']],"baseVolume": [msg['k']['q']],"datetime": [msg['E']], "x": [msg['k']['x']]})
+    global df
+    while msg['k']['x'] == True:
+        data = pd.DataFrame({"close": [msg['k']['c']],"high": [msg['k']['h']],"low": [msg['k']['l']],"open": [msg['k']['o']],"volume": [msg['k']['v']],"baseVolume": [msg['k']['q']],"datetime": [msg['E']]})
+        data['datetime'] = pd.to_datetime(data['datetime'], unit = 'ms')
+        data["datetime"] = data["datetime"].dt.strftime('%Y-%m-%d %H:%M:%S')
         df = df.append(data, ignore_index=True)
-        df["datetime"] = pd.to_datetime(df['datetime'], unit = 'ms')
-        df["datetime"] = df["datetime"].dt.strftime('%Y-%m-%d %H:%M:%S')
         df.to_csv('BTCUSDT.csv')
+        break
     else:
         pass
 
